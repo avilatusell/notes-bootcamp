e7e98559de55e41ec2769a04238d33cae4f8afb2
Anna Vila <avilatusell@gmail.com>
2016-07-10 20:22:28 +0200

    eliminamos el ejecutable jasmine

--- jasmine/jasmine-standalone-2.4.1/spec/isVowelSpec.js
+++
@@ -1,7 +0,0 @@
-// Write a function 'isVowel' that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-
-describe("function isVowel that ", function() {
-    it("should exist", function(){
-        expect( isVowel ).toBeDefined();
-    });
-});
